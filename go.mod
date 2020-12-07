88a4e12affc48a8c7ebea60f16eb6579d57690d7
garcialuis <garcialuisdev@gmail.com>
2020-12-07 10:55:28 -0800

    + Added activity_rabbitmq_service: a rabbitmq messagge consumer, + Dockerfile to build image, *Modified main to run message consumer in a separate goroutine

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/streadway/amqp v1.0.0
 )
--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"github.com/garcialuis/ActivityCollector/api"
+	"github.com/garcialuis/ActivityCollector/api/services"
 )
 
 func main() {
 
+	go func() {
+		services.RunConsumer()
+	}()
+
 	api.Run()
+
 }
